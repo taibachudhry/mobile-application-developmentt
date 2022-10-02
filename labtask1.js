9f4d6009f478284081f5658657d4d49e3e1eb145
taibachudhry <102913808+taibachudhry@users.noreply.github.com>
2022-10-02 23:09:11 +0500

    Add files via upload

---
+++ labtask1.js
@@ -0,0 +1,19 @@
+ var_collection=23617;
+let temp=var_collection;
+let index=parseInt(prompt("Enter index.");
+
+if (k > temp.length)
+    {
+       collection.write(-1);
+    }
+     else
+    {
+        var req = temp.charAt(temp.length - k)
+        collection.write(Number(req));
+    }
+var k = 4;
+
+console.log("print kth digit from last");
+ 
+
+ 
---
+++ labtask2.js
@@ -0,0 +1,11 @@
+let string="23618";
+arrNum=Array.from(string);
+console.log(arrayNum);
+let sum=arr1.reduce(parameter),prev;
+function reducer_func(prev,curr){
+    return Num(prev)+Num(curr);
+}
+console.log(result);
+
+
+
